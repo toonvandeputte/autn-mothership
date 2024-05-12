be037d43907393b19d07937d75e025e4d1e4f7e1
Toon Van de Putte <toon@automaton.be>
2024-05-12 14:10:18 +0200

    map volumes for leaner updates

--- start.sh
+++ start.sh
@@ -6,4 +6,4 @@ echo $SCRIPT_DIR
 docker rm $(docker stop $(docker ps -a -q --filter ancestor=autn-mothership --format="{{.ID}}"))
 
 docker build --tag autn-mothership "$SCRIPT_DIR"
-docker run -p 5050:5050 autn-mothership 
+docker run -v "$SCRIPT_DIR/data":/python-docker/data -v "$SCRIPT_DIR/app/static":/python-docker/app/static -v "$SCRIPT_DIR/app/templates":/python-docker/app/templates -p 5050:5050 autn-mothership 
